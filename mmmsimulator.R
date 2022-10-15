a290efbf281181c9bf00c1e6f2b076d14232012e
Amabile <amabilegaldin11@gmail.com>
2022-10-15 15:07:39 -0300

    teste

--- mmmsimulator.R
+++ mmmsimulator.R
@@ -11,3 +11,21 @@ library(dammmdatagen)
 ## BASE EXEMPLO
 mydf <- generateCovariatesData()
 head(mydf)
+
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(scales)
+ret <- generateRetailData()
+#> Joining, by = "date"
+#> Joining, by = "date"
+#> Joining, by = "date"
+#> New names:
+#> * product_a -> product_a...7
+#> * product_b -> product_b...8
+#> * product_c -> product_c...9
+#> * product_a -> product_a...10
+#> * product_b -> product_b...11
+#> * ...
+dates <- ret[["covariates"]][["Macro"]][["date"]]
+qplot(dates, ret[["response"]]) + geom_line() + ylim(0, NA)
