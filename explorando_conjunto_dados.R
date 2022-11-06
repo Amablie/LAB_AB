fbf84f0455e0429830d4f084d0b8176165dfded5
Cynthia Vidal <cynthiasvidal@gmail.com>
2022-11-06 04:27:36 +0000

    atualização cynthia

--- explorando_conjunto_dados.R
+++ explorando_conjunto_dados.R
@@ -14,11 +14,25 @@ str(data_cyn)
 ##atribuindo indice a cada data
 data_cyn$index <- seq.int(nrow(data_cyn))
 
-str(data_cyn)
+##atribuindo indice à semana
+semana<-as.vector(rep(c(1:373), each = 7))
+indice_semana<-semana[1:2613]
+data_cyn$indice_semana<-indice_semana
+
 ##desconsiderando dados macroeconomicos e GRPs
-only_data<-data_cyn[,c(1,2,3,7,8,9,10,11,12,13,14,21,22,23,24)]
+only_data<-data_cyn[,c(1,2,3,7,8,9,10,11,12,13,14,21,22,23,24,25)]
 str(only_data)
 
-##agregando de forma semanal, somando
+##agregando de forma semanal, somando algumas variaveis, mas manter fixo outras
 
-aggregate(no~DATE+, only_data, sum)
+x<-only_data %>%
+  group_by(indice_semana) %>%
+  summarise(weekly_demand = sum(DEMAND),
+            weekly_mean_unit_price=mean(Unit_Price),
+            weekly_supply_data=sum(Supply_Data),
+            weekly_sales=sum(SALES),
+            weekly_cost_sms=sum(Cost_SMS),
+            weekly_cost_newspapes=sum(Cost_Newspaper),
+            weekly_cost_radio=sum(Cost_Radio),
+            weekly_cost_tv=sum(Cost_TV))
+
