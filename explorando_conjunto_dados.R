ef7ba8c9a084bb8d84ded3f3a218f6b18d777d5f
Amabile <amabilegaldin11@gmail.com>
2022-11-06 00:31:16 -0300

    facet grafico

--- explorando_conjunto_dados.R
+++ explorando_conjunto_dados.R
@@ -14,15 +14,8 @@ str(data_cyn)
 ##atribuindo indice a cada data
 data_cyn$index <- seq.int(nrow(data_cyn))
 
-<<<<<<< HEAD
 
 
-data_cyn %>% 
-  ggplot(aes(x = DATE,
-             y = Cost_SMS))+
-  geom_line() +
-  facet_wrap(~ year)
-=======
 str(data_cyn)
 ##desconsiderando dados macroeconomicos e GRPs
 only_data<-data_cyn[,c(1,2,3,7,8,9,10,11,12,13,14,21,22,23,24)]
@@ -31,4 +24,21 @@ str(only_data)
 ##agregando de forma semanal, somando
 
 aggregate(no~DATE+, only_data, sum)
->>>>>>> 92ffaf2c39598550588e4a2f4b614e95286bd588
+
+data_cyn %>% 
+  ggplot(aes(x = DATE,
+             y = Cost_SMS))+
+  geom_line() +
+  facet_wrap(~ year)
+
+
+# <<<<<<< HEAD
+
+
+data_cyn %>% 
+  ggplot(aes(x = DATE,
+             y = Cost_SMS))+
+  geom_line() +
+  facet_wrap(~ year)
+# =======
+
