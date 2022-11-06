04a09133c7bc64e190d3f4092e2b69fc3b12e12e
Cynthia Vidal <cynthiasvidal@gmail.com>
2022-11-06 02:05:43 +0000

    incluindo novos

--- explorando_conjunto_dados.R
+++ explorando_conjunto_dados.R
@@ -1,16 +1,20 @@
 str(MMM_data)
 data_cyn<-MMM_data
 head(data_cyn)
+str(data_cyn)
 
 ## separadando as datas em dia, mês e ano
 library(tidyverse)
 data_cyn$DATE <- as.Date(data_cyn$DATE)
-data_cyn<-data_cyn%>%transform(data_cyn, day = format(DATE, "%d"), 
+str(data_cyn)
+data_cyn<-data_cyn%>%transform(data_cyn$DATE, day = format(DATE, "%d"), 
           month = format(DATE, "%m"), year = format(DATE, "%Y"))
-data_cyn
-head(data_cyn)
+str(data_cyn)
 
 ##atribuindo indice a cada data
 data_cyn$index <- seq.int(nrow(data_cyn))
-data_cyn
 
+str(data_cyn)
+##desconsiderando dados macroeconomicos e GRPs
+only_data<-data_cyn[,c(1,2,3,7,8,9,10,11,12,13,14,21,22,23,24)]
+str(only_data)
