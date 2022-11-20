3959908537d7dd14a3413b28354828bde4c8f75d
Amabile <amabilegaldin11@gmail.com>
2022-11-19 21:07:53 -0300

    ajuste

--- Series Temporais.R
+++ Series Temporais.R
@@ -5,41 +5,51 @@
 library(astsa) # para analisar séries temporais
 library(tsibble)  # para trabalhar com séries temporais
 library(tidyverse) ### para manipulação e tratamento dos dados
+install.packages("forecast")
+library(forecast)
+
 
 ### foi decidido que a abordagem seria mensal, por conta dos dados macro economicos e do ruído dos dados
 ##### BASE DE DADOS MENSAL ####################################
 
-only_data2<-data_cyn2[,c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,20)]
-data_mes<-only_data2 %>%
-  group_by(anomes) %>%
-  summarise(m_demand = sum(DEMAND),
-            m_mean_unit_price=mean(Unit_Price),
-            m_supply_data=sum(Supply_Data),
-            m_sales=sum(SALES),
-            m_cost_sms=sum(Cost_SMS),
-            m_cost_newspapers=sum(Cost_Newspaper),
-            m_cost_radio=sum(Cost_Radio),
-            m_cost_tv=sum(Cost_TV),
-            m_cost_internet = sum(Cost_Internet),
-            m_CPI=mean(CPI),
-            m_CCI=mean(CCI),
-            m_PPI=mean(PPI))
+
+library(readxl)
+dados_mensais <- read_excel("dados_mensais.xlsx")
+
+str(dados_mensais)
+summary(dados_mensais)
 
 ##############################################################
 
 
 library(lubridate) #incerindo indice para observações do mês
-data_mes$ind_mes<-seq.int(nrow(data_mes))
-data_mes
+dados_mensais$ind_mes<-seq.int(nrow(dados_mensais))
+dados_mensais
 
 ### variaveis resposta
-ggplot(data_mes, aes(x = ind_mes, y = m_demand)) +
+ggplot(dados_mensais, aes(x = ind_mes, y = m_demand)) +
   geom_line()
 
-ggplot(data_mes, aes(x = ind_mes, y = m_sales)) +
+ggplot(dados_mensais, aes(x = ind_mes, y = m_sales)) +
   geom_line()
 
 
-hist(data_mes$m_demand)
-hist(data_mes$m_cost_sms)
-hist(data_mes$m_cost_internet)
+hist(dados_mensais$m_demand)
+hist(dados_mensais$m_cost_sms)
+hist(dados_mensais$m_cost_internet)
+hist(dados_mensais$m_cost_radio)
+hist(dados_mensais$m_cost_tv)
+
+dados_mensais %>% 
+  mutate(mes = as.numeric(somes))
+str(dados_mensais)
+
+
+#facet
+dados_mensais %>% 
+  ggplot(aes(x = somes, y =m_demand, color = factor(soano)))+
+  geom_line()
+
+###########################################################################
+
+auto.arima(dados_mensais$m_demand)
