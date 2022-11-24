4381c5a81f7827fe095f67c238e31e4dd4f03f0f
Amabile <amabilegaldin11@gmail.com>
2022-11-23 23:08:00 -0300

    links

--- Redes neurais.R
+++ Redes neurais.R
@@ -1,2 +1,5 @@
+# https://www.learnbymarketing.com/tutorials/neural-networks-in-r-tutorial/
+# https://www.analyticsvidhya.com/blog/2017/09/creating-visualizing-neural-network-in-r/
+
 #install.packages("neuralnet ")
 library(neuralnet)
