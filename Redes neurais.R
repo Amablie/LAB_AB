9e46378c5c21d8232beec747f02dbd29fbd65e04
Amabile <amabilegaldin11@gmail.com>
2023-01-06 15:52:27 -0300

    redes neurais

--- Redes neurais.R
+++ Redes neurais.R
@@ -6,49 +6,24 @@ library(neuralnet)
 library(keras) # for deep learning
 library(tidyverse) # general utility functions
 library(caret)
+library(readxl)
+
+MMM_data <- read_excel("MMM_data_excel.xlsx", 
+                       col_types = c("text", "date", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric", 
+                                     "numeric", "numeric", "numeric","numeric"))
 
 
-dados_mensais <- read_excel("dados_mensais.xlsx")
-head(dados_mensais)
-str(dados_mensais)
-summary(dados_mensais)
+str(MMM_data)
+head(MMM_data)
+dim(MMM_data)
 
-
-############################# NOVA ESCALA ########################
-
-data_new_escala <-
-  dados_mensais %>% 
-  transform(m_demand = m_demand/1000,
-            m_supply_data  = m_supply_data/1000,
-            m_mean_unit_price = m_mean_unit_price/1000,
-            m_sales = m_sales/1000,
-            m_cost_sms = m_cost_sms/1000,
-            m_cost_newspapers = m_cost_newspapers/1000,
-            m_cost_radio = m_cost_radio/1000,
-            m_cost_tv  = m_cost_tv/1000,
-            m_cost_internet  = m_cost_internet/1000,
-            m_CPI = m_CPI/1000,
-            m_CCI  = m_CCI/1000,
-            m_PPI  = m_PPI/1000)
-
-
-str(data_new_escala)
-
-
-summary(data_new_escala)
-
-
-str(data_new_escala)
-summary(data_new_escala)
-
-
-data_ts_scale<-ts(data_new_escala, frequency=12, start=c(2010,1))
-str(data_ts_scale)
-head(data_ts_scale)
-str(data_ts_scale)
-
-#################################################################
-
+### Devemos manter os dados originais ou transformamos em mensal tbm?
+### https://blogs.rstudio.com/ai/posts/2017-12-20-time-series-forecasting-with-recurrent-neural-networks/
+### Se a resposta for sim, devemos transformar os dados novamente dividindo por 10000 e tirando as colunas não númericas
 
 
 
